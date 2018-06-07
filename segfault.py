8222fcb68927256f8240998562ee42e87bb8e8e2
Joshua Nelson <jyn514@gmail.com>
2018-06-07 19:35:54 -0400

    aren't segfaults lovely? muahaha

---
+++ segfault.py
@@ -0,0 +1,31 @@
+import sys
+from utils import print_file
+
+print('oh come on, you knew running this was a bad idea')
+print_file(__file__)
+
+def recurse(n=0):
+    if n == 0:
+        print("yup, that's 0 alright")
+        return
+    recurse(n - 1)
+
+try:
+    recurse(1000)
+except RecursionError as e:
+    print(e)
+    print("Really? that wasn't even that big!")
+    print("ok let's do this for real")
+
+# found this from trial and error, I think print calls some built-in functions?
+overhead = 5
+sys.setrecursionlimit(1000 + overhead)
+recurse(1000)
+print("that's what I'm talking about")
+
+print("how high can we go?")
+for i in range(10):
+    current = 10 ** i
+    print('trying', current)
+    sys.setrecursionlimit(current + overhead)
+    recurse(current)
